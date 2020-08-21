35dad9722a80a4fe575e496994191a9befe1a876
Morgan Peck <MorganPeck@Morgan-Pecks-MacBook-Pro.local>
2020-08-21 15:25:13 -0500

    removing uneeded content and building out the app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <div class="search">
+        <label for="name">Location</label>
+        <input type="text" name="search" id="search" placeholder="Search" v-model="query" @keypress="fetchForecast">
+      </div>
+      <div class="weather-cards" v-if="typeof weather.main !='undefied'">
+        {{weather.name}}
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data (){
+    return {
+      api_key : '4de4a8d80078d333a4a72f1c11d87820',
+      forecast_url: 'https://api.openweathermap.org/data/2.5/',
+      weather : {},
+      query: '',
+    }
+  },
+  methods: {
+    fetchForecast(e){
+      if(e.key === "Enter"){
+        fetch(`${this.forecast_url}forecast?q=${this.query},us&appid=${this.api_key}`)
+          .then(res => {
+            return res.json();
+            
+          }).then(this.storeData);
+      }
+    },
+
+    storeData(data) {
+      console.log(data.list[1].weather[0].main);
+      this.weather = data;
+    }
+  },
 }
 </script>
 
