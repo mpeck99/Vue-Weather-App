99d329d5d94edd9a4267ddd402b05c08196a027d
Morgan Peck <MorganPeck@Morgan-Pecks-MacBook-Pro.local>
2020-08-21 19:12:02 -0500

    building out the weather data

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,26 @@
     <main>
       <div class="search">
         <label for="name">Location</label>
+        <input type="text" name="search" id="search" placeholder="Search" v-model="query" @keyup.enter="fetchForecast">
         <button @click="fetchForecast" class="searchBtn">Search</button>
       </div>
-      <!-- {{weather.city.name}} -->
       <div class="weather-cards" v-for="weather in weatherData" :key="weather.list">
         <div class="card">
+          <p>{{weather.dt_txt}}</p>
+          <img v-bind:src="'https://openweathermap.org/img/w/' + weather.weather[0].icon +'.png'">
+          <h2>{{weather.weather[0].main}}</h2>
           <p>{{weather.weather[0].description}}</p>
+          <ul>
+            <li>{{weather.main.temp_min}}</li>
+            <li>{{weather.main.temp_max}}</li>
+          </ul>
         </div>
       </div>
     </main>
   </div>
 </template>
 
-<script>
-
+<script>   
 export default {
   name: 'App',
   data (){
@@ -28,19 +34,30 @@ export default {
     }
   },
   methods: {
-    fetchForecast(e){
-      if(e.key === "Enter"){
-        fetch(`${this.forecast_url}forecast?q=${this.query},us&appid=${this.api_key}&units=imperial`)
-          .then(res => {
-            return res.json();
-          }).then(this.storeData);
-      }
+    fetchForecast(){
+     fetch(`${this.forecast_url}forecast?q=${this.query},us&appid=${this.api_key}&units=imperial`)
+      .then(res => {
+        return res.json();
+      }).then(this.storeData);
     },
-      //clearing the data array when a new search is done
-
     storeData(data) {
+      //clearing the data array when a new search is done
+      this.weatherData = [];
+
+      //looping through the data.list to only return the 5 day forecast and not the data for every 3 hours
       for(var i = 0; i < data.list.length; i+=8){
         this.weatherData.push(data.list[i]);
+      }
+      
+      //looping through the weatherDate array to fix temps and dates
+      for(var x = 0; x < this.weatherData.length; x++){
+        var dateMin = parseInt(this.weatherData[x].main.temp_min);
+        var dateMax = parseInt(this.weatherData[x].main.temp_max);
+        var date = new Date(this.weatherData[x].dt_txt.split(" ")[0]).toDateString();
+
+        this.weatherData[x].main.temp_min = dateMin;
+        this.weatherData[x].main.temp_max = dateMax;
+        this.weatherData[x].dt_txt = date;
       }
     }
   },
